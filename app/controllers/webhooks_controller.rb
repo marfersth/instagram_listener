9f643d9bb1d40401406f58160dbbce6593513c71
Augusto Varela <augusto@flimper.com>
2019-09-05 17:05:30 -0300

    Fixes return structure for validation

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,7 +3,7 @@ class WebhooksController < ApplicationController
 
     insta_token = ENV.fetch("INSTAGRAM_VERIFY_TOKEN")
     if insta_token == params["hub.verify_token"]
-      return render json: { challenge: params["hub.challenge"] }
+      return render json: params["hub.challenge"] 
     else
       head :forbidden
     end
