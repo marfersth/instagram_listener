22908e3f8a79bec85de70b9ed677480b3aa12eb5
Augusto Varela <augusto@flimper.com>
2019-09-05 15:39:02 -0300

    Adds controller for webhooks and verification, ignores .env and adds .env.example

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
-  mount RailsAdmin::Engine => '/', as: 'rails_admin'
+  # mount RailsAdmin::Engine => '/', as: 'rails_admin'
+  
+  get '/webhooks', to: 'webhooks#validation'
+
+  post 'webhooks', to: 'webhooks#event'
 
   resources :rules, only: %i[create update destroy]
+
+
 end
