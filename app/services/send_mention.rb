adc45d2dd9b1b0e7fd32ec8d58b39fffb4d45e69
Martin <marfersth@gmail.com>
2019-11-04 14:45:13 -0300

    fix send mention body

--- app/services/send_mention.rb
+++ app/services/send_mention.rb
@@ -7,7 +7,7 @@ class SendMention
 
   def self.mention_and_missing(activity_subscription, subscription, mention)
     response = Subscriptions::Operations::SendCommentAndMention.run!(
-      raw_data: mention.raw_data.to_json,
+      raw_data: mention.raw_data,
       campaign_id: activity_subscription.campaign_id,
       endpoint: subscription.hook_url
     )
